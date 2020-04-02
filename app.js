93f18c6ead1986569d311e88780897b8c694b279
Appleeway <applee62002@gmail.com>
2020-04-02 22:29:43 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
 // const value
 const app = express()
@@ -19,6 +21,22 @@ app.set('view engine', 'handlebars')
 // set bodyParser and methodOverride
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// set session
+app.use(session({
+  secret: 'hotcat',
+  resave: 'false',
+  saveUninitialized: 'false'
+}))
+
+//使用passport - 要比router之前
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 // set router
 app.get('/', (req, res) => {
